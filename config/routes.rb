553380a1462e52fcf9d53368dc803b0b5d878f83
Erin Marshall <erin.e.mars@gmail.com>
2019-02-28 12:09:57 -0500

    Setup index and show api routes for joblistings

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: {format: :json} do
+    resources :job_listings, only: [:index, :show]
+  end
+
+  root to: 'static_pages#root'
+
+
+
+
 end
